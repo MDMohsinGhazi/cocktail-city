9971018078a509b6e4e2b53fbbcb6522249698ff
MD Mohsin Ghazi <mohsin.ghazi379@gmail.com>
2022-08-06 13:14:58 +0530

    add Not Found

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,19 @@
 import { useSelector } from "react-redux";
 import Card from "./Card";
+import NotFound from "../assets/notFound.gif";
 
 const List = () => {
   const { cocktails } = useSelector((state) => state.cocktail);
+
+  if (!cocktails) {
+    return (
+      <section className="flex flex-col justify-center items-center">
+        <h1 className="text-Nutmeg text-2xl lg:text-4xl">No Result Found</h1>
+        <img src={NotFound} alt="" />
+      </section>
+    );
+  }
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
       {cocktails?.map((cocktail, index) => (
